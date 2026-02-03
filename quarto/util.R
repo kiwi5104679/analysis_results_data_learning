c7b22d09f87b419e35054330653ef45f9f92efbe
Shunsuke <kiwikirymymakeup5108144679@gmail.com>
2026-02-03 13:30:22 +0000

    updated

---
+++ quarto/util.R
@@ -0,0 +1,20 @@
+install.packages("rmarkdown")
+install.packages("cards")
+install.packages("dplyr")
+install.packages("webr")
+install.packages("pharmaverseadam")
+
+
+ard <- cards::ard_stack_hierarchical(
+  data = adae,
+  by = TRT01A,
+  variables = c(AEBODSYS, AEDECOD),
+  denominator = adsl,
+  id = USUBJID,
+  over_variables = TRUE
+)
+ard
+library(dplyr)
+library()
+view(ard)
+adae$TRT01A %>% distinct(AEBODSYS)
